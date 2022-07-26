5f5094e670f4f9db41c8120f20a28866fafc102f
Gino Hong <ginohong7@gmail.com>
2022-07-26 04:51:04 +0000

    Completed POST /api/grades.

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -37,3 +37,36 @@ app.get('/api/grades', (req, res) => {
       });
     });
 });
+
+const expressJSON = express.json();
+app.use(expressJSON);
+
+app.post('/api/grades', (req, res) => {
+  if (req.body.name === undefined || req.body.course === undefined || req.body.score === undefined) {
+    res.status(400).json({
+      error: 'Missing name, course, or score'
+    });
+  } else if (req.body.score < 0 || req.body.score > 100) {
+    res.status(400).json({
+      error: 'Score must be an integer from 0 to 100.'
+    });
+  } else {
+    const sql = `
+    insert into "grades" ("name", "course", "score")
+      values ($1, $2, $3)
+    returning *
+  `;
+    const params = [req.body.name, req.body.course, req.body.score];
+    db.query(sql, params)
+      .then(result => {
+        const grade = result.rows[0];
+        res.status(201).json(grade);
+      })
+      .catch(err => {
+        console.error(err);
+        res.status(500).json({
+          error: 'An unexpected error occured.'
+        });
+      });
+  }
+});
